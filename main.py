87aabb9ecff3da353eb170824cef26c68229649b
fan9704 <cxz123499@gmail.com>
2022-03-04 15:34:02 +0800

    Create Big Bullet Class

--- main.py
+++ main.py
@@ -233,6 +233,21 @@ class Bullet(pygame.sprite.Sprite):
         if self.rect.bottom < 0:
             self.kill()
 
+class BigBullet(pygame.sprite.Sprite):#TODO: BIG
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = bullet_img
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.bottom = y
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
+
 class Explosion(pygame.sprite.Sprite):
     def __init__(self, center, size):
         pygame.sprite.Sprite.__init__(self)
